54c1a528dcadecff5d50224713c6a9ba55868184
indubhavaniR <143284543+indubhavaniR@users.noreply.github.com>
2023-09-28 08:44:45 -0500

    homework project completed

--- src/hp_4.py
+++ src/hp_4.py
@@ -10,6 +10,7 @@ MONTHS = {
     9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'
 }
 
+
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
         element to a format dd mmm yyyy--01 Jan 2001."""
@@ -36,49 +37,40 @@ def date_range(start, n):
 
 
 def add_date_range(values, start_date):
-    """
-    Adds a daily date range to the list `values` beginning with
-    `start_date`. The date, value pairs are returned as tuples
-    in the returned list.
-
-    Args:
-        values (list): A daily sequence of numerical values.
-        start_date (str): A date string with format yyyy-mm-dd representing the start date.
-
-    Returns:
-        list: A list of tuples where each tuple contains (date, value).
-    """
-    # Generate the date range starting from start_date
     date_objects = date_range(start_date, len(values))
+    result = [(date_objects[i], values[i]) for i in range(len(values))]
+    return result
 
-    # Pair each date with the corresponding value
-    date_value_pairs = list(zip(date_objects, values))
-    return date_value_pairs
+
+def calculate_late_fee(due_date, return_date):
+    due_date = datetime.strptime(due_date, '%m/%d/%Y')
+    return_date = datetime.strptime(return_date, '%m/%d/%y')
+    days_late = max(0, (return_date - due_date).days)
+    return round(days_late * 0.25, 2)
+
 
 def fees_report(infile, outfile):
-    """Calculates late fees per patron id and writes a summary report to outfile."""
-    # Dictionary to store late fees per patron
-    late_fees_per_patron = defaultdict(float)
+    # Dictionary to store late fees for each patron_id
+    late_fees = defaultdict(float)
 
-    # Read the input CSV file
-    with open(infile, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            date_returned = datetime.strptime(row['date_returned'], '%m/%d/%y')
-            date_due = datetime.strptime(row['date_due'], '%m/%d/%Y')
-            late_days = max(0, (date_returned - date_due).days)
-            late_fee = late_days * 0.25
-            patron_id = row['patron_id']
-            late_fees_per_patron[patron_id] += late_fee
+    # Read CSV file and calculate late fees
+    with open(infile, 'r') as file:
+        reader = csv.DictReader(file)
+        late_fees = defaultdict(float, {
+            row['patron_id']: late_fees[row['patron_id']] + calculate_late_fee(row['date_due'], row['date_returned'])
+            for row in reader
+        })
 
-    # Write the summary report to the output CSV file
-    with open(outfile, 'w', newline='') as csvfile:
-        fieldnames = ['patron_id', 'late_fees']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for patron_id, late_fee in late_fees_per_patron.items():
-            writer.writerow({'patron_id': patron_id, 'late_fees': '{:.2f}'.format(late_fee)})
+    # Write the summary report to the output file
+    with open(outfile, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['patron_id', 'late_fees'])
+        for patron_id, fee in late_fees.items():
+            writer.writerow([patron_id, "{:.2f}".format(fee)])
 
+
+# Example usage
+fees_report('book_returns.csv', 'late_fees_summary.csv')
 
 # The following main selection block will only run when you choose
 # "Run -> Module" in IDLE.  Use this section to run test code.  The
@@ -88,7 +80,7 @@ def fees_report(infile, outfile):
 # under the data directory.
 
 if __name__ == '__main__':
-    
+
     try:
         from src.util import get_data_file_path
     except ImportError:
